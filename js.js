ffc681b7502ee67a7e1f7f9a8ab5d9ca414cec39
aasiskrk <aasis.krk1@gmail.com>
2024-04-13 20:04:25 +0545

    updated contact.html and contact.css along with js.js

--- js.js
+++ js.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Your JavaScript code here
-    document.getElementById("cards").onmousemove = e => {
+    document.getElementById("cards").onmousemove = (e) => {
         for (const card of document.getElementsByClassName("card")) {
             const rect = card.getBoundingClientRect(),
                 x = e.clientX - rect.left,
                 y = e.clientY - rect.top;
-
             card.style.setProperty("--mouse-x", `${x}px`);
             card.style.setProperty("--mouse-y", `${y}px`);
-        };
-    }
+
+            // Exclude links from the mouse event listener
+            const links = card.getElementsByTagName("a");
+            for (const link of links) {
+                link.style.setProperty("--mouse-x", "");
+                link.style.setProperty("--mouse-y", "");
+            }
+        }
+    };
 });
